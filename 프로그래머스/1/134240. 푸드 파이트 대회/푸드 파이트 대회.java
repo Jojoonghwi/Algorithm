f9c1e973c9dc4b0361da1a0334d4d3b326fc83cf
Jojoonghwi <42165254+Jojoonghwi@users.noreply.github.com>
2025-02-20 16:22:17 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 49.79 ms, Memory: 99.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        String foodLeft = "";
+        String foodRight = "";
+
+        for (int i = 1; i < food.length; i++) {
+            for (int j = i; j < food[i] / 2 + i; j++) {
+                foodLeft += i;
+                foodRight = i + foodRight;
+            }
+        }
+        return answer = foodLeft + "0" + foodRight;
+    }
+}
