67606f89da1ffb4785e348cb232bca4ed799bfb1
Jojoonghwi <42165254+Jojoonghwi@users.noreply.github.com>
2025-02-20 16:28:49 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 94.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        char[] alphabet = new char[s.length()];
+        boolean flag = true;
+        for (int i = 0; i < s.length(); i++) {
+            alphabet[i] = s.charAt(i);
+            if (alphabet[i] == ' ') {
+                flag = true;
+                continue;
+            }
+                     
+            if (flag) {
+                if(alphabet[i] > 96 && alphabet[i] < 123) {
+                    alphabet[i] -= 32;
+                }
+                flag = false;
+
+            }
+            else {
+                if(alphabet[i] > 64 && alphabet[i] < 91) {
+                    alphabet[i] += 32;
+                }
+                flag = true;
+            }
+            
+        }
+        
+        return answer = new String(alphabet);
+    }
+}
