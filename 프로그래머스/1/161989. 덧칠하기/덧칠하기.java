1a01f1ded8a567524f41aea56d27e23176dfb8aa
Jojoonghwi <42165254+Jojoonghwi@users.noreply.github.com>
2025-02-25 14:18:27 +0900

    [level 1] Title: 덧칠하기, Time: 1416.95 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int index=0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < section.length; i++){
+            list.add(section[i]);
+        }
+        while(!list.isEmpty()){
+            index = list.get(0);
+            for(int i=index; i<index+m; i++){
+                if(list.contains(i)){
+                    list.remove(0);
+                }
+                if(list.isEmpty()){
+                    break;
+                }
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
