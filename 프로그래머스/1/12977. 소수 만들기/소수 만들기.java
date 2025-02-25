588166a4ec00e6cbb28691c56ca4aa417c03a0e3
Jojoonghwi <42165254+Jojoonghwi@users.noreply.github.com>
2025-02-25 14:18:19 +0900

    [level 1] Title: 소수 만들기, Time: 27.26 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        List<Integer> sosu = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    sosu.add(nums[i] + nums[j] + nums[k]);
+                }
+            }
+        }
+        
+        int notsosu=0;
+        for (int i = 0; i < sosu.size(); i++) {
+            for (int j = 2; j < sosu.get(i); j++) {
+                if (sosu.get(i) % j == 0) {
+                    notsosu++;
+                    break;
+                }
+            }
+        }
+
+        return answer = sosu.size() - notsosu;
+    }
+}
